d7f917c8b7b75014a76fed96c7e29c90a5e25b34
Muhammad Fuadi <fuadevo19@gmail.com>
2024-08-15 01:27:56 +0700

    menambahkan folder baru

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const listIcon = document.getElementById("list");
+const menuList = doment.getElementById("menuList");
+
+listIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
